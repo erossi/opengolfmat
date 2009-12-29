f31badeeb87983d24209a74ab7ffb9743cfc7890
Enrico Rossi <e.rossi@tecnobrain.com>
2009-12-29 16:05:17 +0100

    changed speed from 0.3 to 2 Khz

--- src/counter.h
+++ src/counter.h
@@ -25,15 +25,15 @@
 #define COUNTER_DELAY_LOOP 100
 
 /* Accelleration and decelleration top and bottom */
-#define COUNTER_TOP_COMPARE 30
-#define COUNTER_BOTTOM_COMPARE 10
+#define COUNTER_TOP_COMPARE 100
+#define COUNTER_BOTTOM_COMPARE 15
 /* Number of steps before changing the treshold during
    accel. and decel. */
 #define COUNTER_LOOP_TIMES 3
 
 /* Accellerate/decellerate treshold. Has to be more than
    2 * (TOP - BOTTOM) * LOOP_MATCH */
-#define COUNTER_STARTSTOP_STEPS 120
+#define COUNTER_STARTSTOP_STEPS 510
 
 void counter_start(void);
 void counter_stop(void);
